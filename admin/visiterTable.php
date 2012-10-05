5aeb06afa9368fa70c4789a56278e9b8b1a55a82
snghrym <i@snghrym.com>
2012-10-05 13:08:10 +0900

    2.0.1 Visiterテーブルの修正

--- admin/visiterTable.php
+++ admin/visiterTable.php
@@ -1,3 +1,4 @@
+
 <?php
 
 require_once "database.php";
@@ -6,31 +7,40 @@ require_once "database.php";
 //既に登録されているかを確認
 $sql = "SELECT * FROM ".DB_NAME.".Visiter;";
 $result = mysql_query($sql);
+?>
 
-echo "<h2>ソーシャルランプのデータベースの『Visiter』テーブルの中身を表示しています。</h2>";
-echo "<p>末端のユーザーが、設置者のソーシャルランプでコネクトしたとき、そのユーザー情報がVisiterテーブルの中に入ります。</p>";
-echo "<p>コネクトする度に、そのユーザーの情報が最新のものに上書きされます。</p>";
-echo "<p>アカウント名@facebook.comというアドレスで、Facebookメッセージが送れます。</p>";
-echo "<p><a href='http://hirayamaru.info/sociallanpWeb/' target='_blank'>こちら</a>から、コネクトのテストをすることができます。</p>";
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta http-equiv="Content-Style-Type" content="text/css">
+	<meta http-equiv="Content-Script-Type" content="text/javascript">
+</head>
+<body>
+<h2>ソーシャルランプのデータベースの『Visiter』テーブルの中身を表示しています。</h2>
+<p>末端のユーザーが、設置者のソーシャルランプでコネクトしたとき、そのユーザー情報がVisiterテーブルの中に入ります。</p>
+<p>コネクトする度に、そのユーザーの情報が最新のものに上書きされます。</p>
+<p>アカウント名@facebook.comというアドレスで、Facebookメッセージが送れます。</p>
+<p><a href='http://hirayamaru.info/sociallanpWeb/' target='_blank'>こちら</a>から、コネクトのテストをすることができます。</p>
 
-echo "<table border=1>";
+<table border=1>
 
-echo "<tr>";
+<tr>
 
-echo "<th>ID</th>";
-echo "<th>アカウント名</th>";
-echo "<th>名前</th>";
-echo "<th>メールアドレス</th>";
-echo "<th>現住地</th>";
-echo "<th>性別</th>";
-echo "<th>職歴・学歴</th>";
-echo "<th>初回コネクト日</th>";
-echo "<th>最終コネクト日</th>";
-echo "<th>最終取得元アプリID</th>";
-echo "<th>最終取得元ドメイン</th>";
-echo "<th>コネクト回数</th>";
+<th>ID</th>
+<th>アカウント名</th>
+<th>名前</th>
+<th>メールアドレス</th>
+<th>現住地</th>
+<th>性別</th>
+<th>職歴・学歴</th>
+<th>初回コネクト日</th>
+<th>最終コネクト日</th>
+<th>最終取得元アプリID</th>
+<th>最終取得元ドメイン</th>
+<th>コネクト回数</th>
 
-echo "</tr>";
+</tr>
+<?php
 
 
 while ($row = mysql_fetch_assoc($result)) {
@@ -53,8 +63,10 @@ echo "<td>".$row['EntranceName']."</td>";
 echo "<td>".$row['Count']."回</td>";
 
 echo "</tr>";
+}?>
 
-}
-echo "</table>";
 
-?>
+</table>
+
+</body>
+</html>
